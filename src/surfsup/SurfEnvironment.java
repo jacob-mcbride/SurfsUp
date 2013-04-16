6f93ab4753759bb0d91823cbd959f39c36af6517
MaxDavies <max.davies@brentwood.bc.ca>
2013-04-16 10:27:52 -0700

    added environment

---
+++ src/surfsup/SurfEnvironment.java
@@ -0,0 +1,51 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package surfsup;
+
+import environment.Environment;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
+
+/**
+ *
+ * @author MaxDavies
+ */
+public class SurfEnvironment extends Environment {
+
+   
+
+    @Override
+    public void initializeEnvironment() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void timerTaskHandler() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void keyPressedHandler(KeyEvent e) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void keyReleasedHandler(KeyEvent e) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void environmentMouseClicked(MouseEvent e) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void paintEnvironment(Graphics graphics) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
--- src/surfsup/SurfsUp.java
+++ src/surfsup/SurfsUp.java
@@ -4,6 +4,12 @@
  */
 package surfsup;
 
+import environment.ApplicationStarter;
+import environment.Environment;
+import image.ResourceTools;
+import java.awt.Dimension;
+import java.awt.Image;
+
 /**
  *
  * @author user
@@ -19,6 +25,31 @@ public class SurfsUp {
     
     public static void start(){
         System.out.println("Surf's Up!");
+<<<<<<< HEAD
         System.out.println("Ben rocks!");
+=======
+         String[] args =  new String[0];
+       //NOTE: you can change the string below to change the window title
+       String appName = "Surfs Up!";
+       //NOTE: you can change the numbers below (800, 600) to change the size of the window
+       Dimension appSize = new Dimension(800, 600);
+       
+       // Get image for background
+       // Image background = ResourceTools.loadImageFromResource("resource/grass.jpg");    
+       
+    //   Image background = ResourceTools.loadImageFromResource("resources/sammy.jpg");
+       Image background = ResourceTools.loadImageFromResource("resources/wave.jpeg");
+       
+       
+       
+
+       // NOTE: you MUST create a new environment that using the "xxxxEnvironment" class from your application,
+       // i.e. it will almost certainly NOT be called "SnakeBiteEnvironment", but rather
+       // something like "SpaceEnvironment"
+       Environment environment = new SurfEnvironment();
+       
+       ApplicationStarter.run(args, appName, appSize, environment);  
+       
+>>>>>>> added environment
     }
 }
